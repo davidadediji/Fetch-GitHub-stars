a99c6c5c0caf6595d7f4535c8cbacb410cefa900
davidadediji <davidadediji@gmail.com>
2023-06-22 13:50:19 +0100

    chore: add create and delete note functionality

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,32 @@ export default{
  },
 
  data(){
-  let id = 0
+ 
   return {
-    notes:[
-      {title:'exhausting', description:'really exhausting i was not expecting it at all', id:id++},
-      { title: 'sticky reminder', description: 'really exhausting i was not expecting it at all', id: id++ },
-      { title: 'grab a chicken', description: 'really exhausting i was not expecting it at all', id: id++ },
-    ]
+    notes:[],
+    id:0
+  }
+ }, 
+
+ mounted(){
+  const savedNotes = localStorage.getItem('notes');
+  if(savedNotes){
+    this.notes = JSON.parse(savedNotes)
+  }
+ },
+ methods:{
+  createNote(title, description){
+    if(title !== '' & description !== ''){
+      this.notes.push({ title: title, description: description, id: this.id++ })
+      this.saveToDb();
+    }
+  },
+  deleteNote(note){
+    this.notes = this.notes.filter((t)=>t !== note)
+    this.saveToDb()
+  },
+  saveToDb(){
+    localStorage.setItem('notes', JSON.stringify(this.notes))
   }
  }
 }
@@ -25,15 +44,16 @@ export default{
       <p class=" font-bold text-lg text-green-900">C.W Notes</p>
     </nav>
     <section>
-      <form-input></form-input>
+      <form-input @add="createNote"></form-input>
     </section>
     <section>
-  <ul class=" max-w-xl m-auto flex flex-col gap-3">
+  <ul v-if="notes.length > 0" class=" max-w-xl m-auto flex flex-col gap-3">
+    <li class="font-medium text-lg">All Notes</li>
         <li v-for="note in notes" :key="note.id" class="border border-green-400 py-2 px-4 rounded-lg flex justify-between">
          <p> {{ note.title }}</p>
          <div class="flex gap-4">
           <button>edit</button>
-          <button>delete</button>
+          <button @click="deleteNote(note)">delete</button>
          </div>
         </li>
       </ul>
