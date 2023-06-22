3f5a436bb03338cb4a2373a7057ba445978970af
davidadediji <davidadediji@gmail.com>
2023-06-22 11:34:51 +0100

    chore:adjust color

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,44 @@
-<script setup>
+<script>
+import NoteCreate from './components/NoteCreate.vue';
+export default{
+ components:{
+  'form-input':NoteCreate
+ },
+
+ data(){
+  let id = 0
+  return {
+    notes:[
+      {title:'exhausting', description:'really exhausting i was not expecting it at all', id:id++},
+      { title: 'sticky reminder', description: 'really exhausting i was not expecting it at all', id: id++ },
+      { title: 'grab a chicken', description: 'really exhausting i was not expecting it at all', id: id++ },
+    ]
+  }
+ }
+}
 
 </script>
 
 <template>
-  <main class="flex flex-col justify-between border border-blue-300">
-   <div>David Adediji</div>
-   <p>Doyin digji</p>
+  <main class="flex flex-col gap-12">
+    <nav class=" p-2 sticky">
+      <p class=" font-bold text-lg text-green-900">C.W Notes</p>
+    </nav>
+    <section>
+      <form-input></form-input>
+    </section>
+    <section>
+  <ul class=" max-w-xl m-auto flex flex-col gap-3">
+        <li v-for="note in notes" :key="note.id" class="border border-green-400 py-2 px-4 rounded-lg flex justify-between">
+         <p> {{ note.title }}</p>
+         <div class="flex gap-4">
+          <button>edit</button>
+          <button>delete</button>
+         </div>
+        </li>
+      </ul>
+    </section>
+    
   </main>
 </template>
 
---
+++ src/components/NoteCreate.vue
@@ -0,0 +1,35 @@
+<script>
+export default{
+    data(){
+        return {
+            title:'',
+            description:''
+        }
+    },
+    methods:{
+        add(){
+            this.$emit=this.title
+            this.$emit=this.description
+
+        }
+    }
+
+}
+</script>
+
+<template>
+    <section class="m-auto max-w-xl border border-b-4 border-green-400 rounded-lg p-2">
+        <form action="" @submit.prevent="add" method="post">
+            <section class="flex flex-col">
+                <label for="title"></label>
+                <input type="text" name="title" id="title" class="text-gray-900 outline-none px-2 py-2 bg-slate-100" placeholder="Title" v-model="title">
+                <textarea name="" id="" cols="30" rows="8" placeholder="Take notes here..." class="px-2 outline-none pt-2 bg-slate-100" v-model="description"></textarea>
+                <span class=" text-xs text-gray-500 pl-2">{{ description.split(' ').filter(word => word !== '').length }} words written</span>
+                
+            </section>
+            <section class="w-full text-right">
+                <button type="submit" class=" text-sm px-4 py-2 text-right mt-3 border border-green-400 hover:bg-green-400 hover:text-white rounded text-gray-600">Create</button>
+            </section>
+        </form>
+    </section>
+</template>
